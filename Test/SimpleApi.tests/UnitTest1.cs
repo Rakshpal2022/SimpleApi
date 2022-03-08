d7510423a74b4c12d7a19bb9654cfadac5025024
Rakshpal2022 <abhysinghlko@gmail.com>
2022-03-08 13:39:40 +0530

    first time

---
+++ Test/SimpleApi.tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using SimpleApi;
+using SimpleApi.Controllers;
+
+namespace SimpleApi.tests
+{
+    public class UnitTest1
+    {
+        ValuesController objValueController=new ValuesController();
+        [Fact]
+        public void Test1()
+        {
+        var ctr=objValueController.Get(1);
+        Assert.Equal("Raksh Pal",ctr);
+        }
+    }
+}
